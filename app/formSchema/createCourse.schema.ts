1737f67589a6e0fc177550eb8eb2056dad1d2fea
Manoj Santra <santramanoj1997@gmail.com>
2024-03-01 06:57:33 +0530

    uploadthing integration complete

--- app/formSchema/createCourse.schema.ts
+++ app/formSchema/createCourse.schema.ts
@@ -3,6 +3,9 @@ import * as z from "zod";
 export const CrateCourseSchema = z.object({
   title: z.string().min(1, { message: "Title is required" }),
 });
+export const CrateCourseDescriptionSchema = z.object({
+  description: z.string().min(1, { message: "Description is required" }),
+});
 
 // export const createTitleForCourseSchema=z.object({
 //   title:
---
+++ components/ui/file-upload.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { UploadDropzone } from "@/lib/UploadThing";
+import { ourFileRouter } from "@/app/api/uploadthing/core";
+import { FC, Fragment } from "react";
+import toast from "react-hot-toast";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  endpoint: keyof typeof ourFileRouter;
+}
+
+export const FileUpload: FC<FileUploadProps> = ({
+  onChange,
+  endpoint,
+}): JSX.Element => {
+  return (
+    <Fragment>
+      <UploadDropzone
+        endpoint={endpoint}
+        onClientUploadComplete={(res) => {
+          onChange(res?.[0].url);
+        }}
+        onUploadError={(error: Error) => {
+          toast.error(`${error.message}`);
+        }}
+      />
+    </Fragment>
+  );
+};
